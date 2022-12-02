0766ca3cff332dc71e0959e4c04b0a3680868035
nilsbayer <112652713+nilsbayer@users.noreply.github.com>
2022-12-02 20:56:50 +0100

    fix: change loading text

--- static/js/loading.js
+++ static/js/loading.js
@@ -3,7 +3,7 @@ const links = document.querySelectorAll(".lazy-link")
 links.forEach(link => {
     link.addEventListener("click", () => {
 
-        document.querySelector("main").innerHTML = "<h1>Loading data ...</h1><h3>This could take a second.</h3>"
+        document.querySelector("main").innerHTML = "<h1>We currently read and analyse the papers ...</h1><h3>This could take a second.</h3><div><img src='https://i.giphy.com/media/WoWm8YzFQJg5i/giphy.webp'></div>"
 
     })
 })
